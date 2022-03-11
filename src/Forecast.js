c16898962fd48e6ed22c9b89a61de5662c398b6a
Kristina <crsbka@bk.ru>
2022-03-10 20:53:47 -0500

    Fixed bugs

--- src/Forecast.js
+++ src/Forecast.js
@@ -23,7 +23,7 @@ export default function Forecast(props) {
                 <div className="row">
 
                     {forecast.map(function (dailyForecast, index) {
-                        if (index < 6) {
+                        if (index < 5) {
                             return (
                                 <div className="col" key={index}>
                                     <ForecastDay data={dailyForecast}/>
